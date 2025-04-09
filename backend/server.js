0f3c1a36492bb5bf7ae1e123c6ba78d6681d5c46
Yogavelan-BJ <yogavelanbj@gmail.com>
2025-04-10 01:44:44 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -11,8 +11,12 @@ dotenv.config();
 const app = express();
 
 connectDB();
-
-app.use(cors());
+const corsOptions = {
+  origin: "https://course-gpt-qpa9.vercel.app",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use("/api/modules", moduleRoutes);
